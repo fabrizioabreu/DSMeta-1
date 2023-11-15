01361244a6b9481aab80189806f7e4da67b39ff7
fabrizioabreu <fabrizioabreu@live.com>
2023-11-15 14:10:26 -0300

    Send notification

--- frontend/src/components/NotificationButton/index.tsx
+++ frontend/src/components/NotificationButton/index.tsx
@@ -1,10 +1,22 @@
+import axios from "axios";
 import icon from "../../assets/img/notification-icon.svg";
 
 import "./styles.css";
+import { BASE_URL } from "../../utils/request";
 
-function NotificationButton() {
+type Props = {
+  saleId: number;
+};
+
+function NotificationButton({ saleId }: Props) {
+  function handleClick(id: number) {
+    axios(`${BASE_URL}/sales/${id}/notification`).then((response) => {
+      console.log("SUCESSO");
+    });
+  }
+
   return (
-    <div className="dsmeta-red-btn">
+    <div className="dsmeta-red-btn" onClick={() => handleClick(saleId)}>
       <img src={icon} alt="Notificar" />
     </div>
   );
